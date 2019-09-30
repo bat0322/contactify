b8249fe8519653efd93bc06c7e6e012dc1ccdb3c
zjohnson5455 <zacharyjohnsonri@gmail.com>
2019-09-30 16:28:42 -0400

    two buttons with different jquery selectors for submit and send

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
 $(document).ready(function(){
-  $("button").click(function(){
-    console.log('Connected to JS file');
+  console.log('Connected to JS file');
+  // submit contact info
+  $("#submit").click(function(){
     var choices = $("input[type='text']").map(function(i, val) {
       return $(val).val();
     }).toArray();
     console.log(choices);
+  });
+
+  // send vcf to zack's email
+  $("#send").click(function(){
+    console.log("sending contact info");
   });
 });
