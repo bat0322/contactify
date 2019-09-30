9903afef0d135076904f46792c3d4a8d737a72fb
zjohnson5455 <zacharyjohnsonri@gmail.com>
2019-09-30 17:16:27 -0400

    adding post request code

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ $(document).ready(function(){
       return $(val).val();
     }).toArray();
     console.log(choices);
+    sendContact(choices);
   });
 
   // send vcf to zack's email
@@ -13,3 +14,25 @@ $(document).ready(function(){
     console.log("sending contact info");
   });
 });
+
+
+function sendContact(choices) {
+  $.ajax({
+      method: 'POST',
+      url: _config.api.invokeUrl + '/contact',
+      headers: {},
+      data: JSON.stringify({
+          firstName: choices[0],
+          lastName: choices[1],
+          email: choices[2],
+          phoneNumber: choices[3]
+      }),
+      contentType: 'application/json',
+      success: completeRequest,
+      error: function ajaxError(jqXHR, textStatus, errorThrown) {
+          console.error('Error sending contact info: ', textStatus, ', Details: ', errorThrown);
+          console.error('Response: ', jqXHR.responseText);
+          alert('An error occured when sending your contact information:\n' + jqXHR.responseText);
+      }
+  });
+}
